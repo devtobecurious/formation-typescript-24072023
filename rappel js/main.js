598f855bec60ced4f763e781cd84ce06658fe978
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-24 10:37:53 +0200

    feat: demarrage journée

---
+++ rappel js/main.js
@@ -0,0 +1,38 @@
+
+
+/**
+ * Création d'un objet Hobbit
+ */
+class Hobbit {
+    #_nom = 'coucou';
+
+    constructor(nom, age) {
+        this.nom = nom;
+        this.age = age;
+        this.adulte = false;
+    }
+
+    seDeplacer(afficher) {
+        afficher(`${this.nom} se déplace`);
+    }
+
+    //#region getters/setters
+    get nom() {
+        return this.#_nom;
+    }
+
+    set nom(nom) {
+        this.#_nom = nom;
+    }
+    //#endregion
+}
+
+let frodon = new Hobbit('Frodon', 33);
+frodon.nom = 'coucou';
+frodon.nom = 1;
+frodon.seDeplacer(() => {
+    console.log('coucou');
+});
+frodon.seDeplacer('truc');
+
+frodon = 1;
