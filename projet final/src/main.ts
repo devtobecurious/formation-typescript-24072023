ba07cbba0f6ee996d856fb9f2031535dd13f4835
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-24 14:58:53 +0200

    fix: avec un type guard

--- projet final/src/main.ts
+++ projet final/src/main.ts
@@ -3,12 +3,15 @@ import { Elfe } from "./elfe";
 import { Hobbit } from "./hobbit";
 
 const resultPseudo = prompt('Prenom ?');
-const personnage: Personnage = new Hobbit(resultPseudo, 100);
+
+if (resultPseudo) { // type guard
+    const personnage: Personnage = new Hobbit(resultPseudo, 100);
+}
 
 
 function afficherInfosPersonnage(personnage: Personnage) {
     console.log(`${personnage.pseudo} a ${personnage.pointsDeVie} points de vie`);
 }
 
-afficherInfosPersonnage(personnage);
+// afficherInfosPersonnage(personnage);
 // afficherInfosPersonnage(new Elfe('Legolas', 200));
